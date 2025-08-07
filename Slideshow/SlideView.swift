8aa3ccbee6f25de28283dedb8edde50012fc1f4a
Paul Darcey <github@xerodonia.com>
2025-08-07 15:42:10 +1000

    Implemented key actions

--- Slideshow/SlideView.swift
+++ Slideshow/SlideView.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import Combine
 
 struct SlideView: View {
     var images: [Image]
+    var autoModeInterval: Double
     @State var currentImage: Int = 0
     @Binding var slideshowIsRunning: Bool
     @FocusState private var focussed: Bool
+    @AppStorage("showMetadata") var showMetadata = true
+
+    // Auto Mode
+    @AppStorage("autoModeInterval") var interval: Double = 3.0
+    @AppStorage("autoMode") var autoMode: Bool = true
+    let timer: Publishers.Autoconnect<Timer.TimerPublisher>
+
+    init(images: [Image], autoModeInterval: Double = 3.0, currentImage: Int, slideshowIsRunning: Binding<Bool>) {
+        self.images = images
+        self.autoModeInterval = autoModeInterval
+        self.currentImage = currentImage
+        self._slideshowIsRunning = slideshowIsRunning
+        self.timer = Timer.publish(every: autoModeInterval, on: .main, in: .common).autoconnect()
+    }
 
     var body: some View {
         ZStack {
@@ -21,6 +37,7 @@ struct SlideView: View {
                 .aspectRatio(contentMode: .fit)
                 .focusable()
                 .focused($focussed)
+                .transition(.opacity)
                 .onAppear {
                     focussed = true
                 }
@@ -61,13 +78,46 @@ struct SlideView: View {
                         return .handled
                     }
                 })
+                .onKeyPress(keys: ["h", "H"], action: { _ in
+                    // Show/Hide metadata (i.e. "1 of 100: <filename>")
+                    withAnimation {
+                        showMetadata.toggle()
+                        return .handled
+                    }
+                }
+                )
+                .onKeyPress(keys: ["a", "A"], action: { _ in
+                    // Toggle autoMode
+                    withAnimation {
+                        autoMode.toggle()
+                        return .handled
+                    }
+                }
+                )
+                .onReceive(timer) { _ in
+                    // Automode progress
+                    if autoMode {
+                        guard !images.isEmpty else { return }
+                        withAnimation {
+                            if (currentImage + 1) < images.count {
+                                // Show next slide
+                                currentImage += 1
+                            } else {
+                                // Last slide has been shown, cancel the slideshow
+                                slideshowIsRunning = false
+                                currentImage = 0
+                            }
+                        }
+                    }
+                }
 
-            Text("\(currentImage + 1) of \(images.count)")
-                .font(.subheadline)
-                .foregroundColor(.white)
-                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity, alignment: .topLeading)
-                .padding()
-
+            if showMetadata {
+                Text("\(currentImage + 1) of \(images.count): <filename>")
+                    .font(.subheadline)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                    .padding()
+            }
         }
         .onTapGesture(perform: {
             withAnimation {
@@ -81,9 +131,7 @@ struct SlideView: View {
                 }
             }
         })
-
     }
-
 }
 
 //#Preview {
